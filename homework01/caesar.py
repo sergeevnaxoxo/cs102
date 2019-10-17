2b6682a9faf7923d0466dbbfee113c3d26d9fb0e
sergeevnaxoxo <56534248+sergeevnaxoxo@users.noreply.github.com>
2019-10-17 11:18:36 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,49 +1,56 @@
-def encrypt_caesar(plaintext: str) -> str:
+def encrypt_caesar(plaintext, shift):
     """
     Encrypts plaintext using a Caesar cipher.
-    >>> encrypt_caesar("PYTHON")
+    >>> encrypt_caesar("PYTHON", 3)
     'SBWKRQ'
-    >>> encrypt_caesar("python")
+    >>> encrypt_caesar("python", 3)
     'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
+    >>> encrypt_caesar("Python3.6", 3)
     'Sbwkrq3.6'
-    >>> encrypt_caesar("")
+    >>> encrypt_caesar("", 3)
     ''
     """
-    # PUT YOUR CODE HERE
-    for i in plaintext:
-        if ('a' <= i <= 'z') or ('A' <= i <= 'Z'):
-            if ('a' <= i <= 'w') or ('A' <= i <= 'W'):
-                l = chr(ord(i) + 3)
-            else:
-                l = chr(ord(i) - 23)
-        else:
-            i += l
-        plaintext = plaintext.replace(i, l)
+    ciphertext = ""
+    for char in plaintext:
+        if "A" <= char <= "z":
+            symbol = ord(char) + shift % 26
+
+            if "A" <= char <= "Z" < chr(symbol):
+                symbol -= 26
+
+            elif chr(symbol) > "z":
+                symbol -= 26
+
+            char = chr(symbol)
+        ciphertext += char
+
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext, shift):
+    """
+     Decrypts a ciphertext using a Caesar cipher.
+     >>> decrypt_caesar("SBWKRQ", 3)
+     'PYTHON'
+     >>> decrypt_caesar("sbwkrq", 3)
+     'python'
+     >>> decrypt_caesar("Sbwkrq3.6", 3)
+     'Python3.6'
+     >>> decrypt_caesar("", 3)
+     ''
+     """
+    plaintext = ""
+    for char in ciphertext:
+        if "A" <= char <= "z":
+            enc = ord(char) - shift % 26
+
+            if enc < ord("A"):
+                enc += 26
+
+            if "a" <= char <= "z" and enc < ord("a"):
+                enc += 26
+
+            char = chr(enc)
+        plaintext += char
 
     return plaintext
-
-
-def decrypt_caesar(ciphertext: str) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    # PUT YOUR CODE HERE
-    for i in ciphertext:
-        if ('a' <= i <= 'z') or ('A' <= i <= 'Z'):
-            if ('d' <= i <= 'z') or ('D' <= i <= 'Z'):
-                l = chr(ord(i) - 3)
-            else:
-                l = chr(ord(i) + 23)
-        else:
-            i += l
-        ciphertext = ciphertext.replace(i, l)
-    return ciphertext
